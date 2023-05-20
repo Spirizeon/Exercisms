670763991078e8c7a30dc566c70c8ac5f43f8b6f
Ash <spirizeon@proton.me>
2023-05-20 10:32:16 +0530

    solved leap

--- dart/leap/lib/leap.dart
+++ dart/leap/lib/leap.dart
@@ -1,3 +1,14 @@
 class Leap {
   // Put your code here
+  bool leapYear(int year) {
+    if (year % 4 == 0) {
+      if (year % 100 == 0) {
+        return false;
+      } else {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  }
 }
