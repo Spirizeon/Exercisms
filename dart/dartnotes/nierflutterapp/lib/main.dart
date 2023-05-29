4659b90cd96bb9e140ca28b8f75ce314d56aea9f
Ash <spirizeon@proton.me>
2023-05-29 19:27:34 +0530

    updates

--- dart/dartnotes/nierflutterapp/lib/main.dart
+++ dart/dartnotes/nierflutterapp/lib/main.dart
@@ -11,7 +11,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       // scaffold widget
-      backgroundColor: Colors.white70,
+      backgroundColor: Colors.blue,
       appBar: AppBar(
         //topbar
         title: Text("app",
@@ -24,23 +24,45 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.black12,
       ),
       // body: Text('hello ninjas')),
-      body: Center(
-          // child: Image(
-          //     image: NetworkImage('https://w.wallhaven.cc/full/dg/wallhaven-dg5rpl.png')
-          // ),
+      // body: Center(
+      //     // child: Image(
+      //     //     image: NetworkImage('https://w.wallhaven.cc/full/dg/wallhaven-dg5rpl.png')
+      //     // ),
 
-          // child: ElevatedButton(
-          //      onPressed: () {}, child: Text('click me'), color: Colors.orange)),
+      //     // child: ElevatedButton(
+      //     //      onPressed: () {}, child: Text('click me'), color: Colors.orange)),
 
-          //     child: TextButton(
-          //   onPressed: () {
-          //     // has button code here (can be Backend connection)
-          //     print('you clicked me');
-          //   },
-          //   child: Text('Click ME'),
-          // )
+      //     //     child: TextButton(
+      //     //   onPressed: () {
+      //     //     // has button code here (can be Backend connection)
+      //     //     print('you clicked me');
+      //     //   },
+      //     //   child: Text('Click ME'),
+      //     // )
 
-          ),
+      // body: Container(
+      //   // padding: EdgeInsets.all(20.0), //padding is inner area
+
+      //   // padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+      //   padding: EdgeInsets.fromLTRB(10.0,20.0,30.0,40.0),
+      //   margin: EdgeInsets.all(30.0),
+      //   color: Colors.grey[400], // grey strength 400
+      //   child: Text('Hello'),
+      // ), //container
+
+      body: Row(children: <Widget>[
+        Text('hello world'),
+        TextButton(
+          onPressed: () {},
+          child: Text('click me'),
+        ), //textbutton
+        Container(
+          color: Colors.cyan,
+          padding: EdgeInsets.all(30.0),
+          child: Text('inside container'),
+        ), //container
+      ] //children
+          ), //body
 
       // CAUTION: Changes to Flutter widgets since 2021 updates
       // FlatButton → TextButton
